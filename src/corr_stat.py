a9e3bc40a2b17143b4410b77470be923d4f55f82
unknown <omargluhic932@gmail.com>
2022-03-01 21:42:00 +0100

    Finished corr_statistics

--- src/corr_stat.py
+++ src/corr_stat.py
@@ -1,12 +1,8 @@
 import logging
 import ast
-import json
 from flask_restful import reqparse, Resource
-from src.database import db
-from datetime import datetime, timedelta
+from src.database import DegreeDataTable, db
 import pandas as pd
-import numpy as np
-from src.services.data_requester import GetWeatherDDData
 
 
 #### Logger configuration ####
@@ -24,6 +20,28 @@ corr_analysis_parms.add_argument("consumption", location="json")
 corr_analysis_parms.add_argument("location", location="json")
 
 class DDCorelationAnalysis(Resource):
+    """This endpoint/class allows the user to send in energy consumption
+    data, dates of the consumption and location of the consumption. Afterwards,
+    the function evaluates the data against the degree database and finds the
+    greatest correlation between the degree data groups. This is then returned
+    to the user.
+
+    Inputs:
+        period (str): period of the consumption
+        consumption (str): the consumption of the coresponding period
+        location (str): location of the coresponding consumpion
+
+    Returns:
+        json (json): provides the maximum coefficient of determination and its
+        coresponding degree data group.
+    """
+    COLUMNS_DATA = ['datetime',
+                    'CDD_10_5',
+                    'CDD_15_5',
+                    'CDD_18_5',
+                    'HDD_10_5',
+                    'HDD_15_5',
+                    'HDD_18_5'] 
     
     def __init__(self):
         
@@ -33,12 +51,28 @@ class DDCorelationAnalysis(Resource):
         self.period = self.args["period"]
         self.location = self.args["location"]
         
-    def _date_validator(self, input_date):
+    def _date_validator(self, input_date : str):
         
-        sql_query = f"SELECT datetime FROM degree_data_table "\
-                    f"WHERE datetime = '{input_date}'"
-        return db.session.execute(sql_query).fetchall()
-      
+        """Validator that checks if the database
+        has data coresponding to the requested date
+
+        Args:
+            input_date (str): loop feed data from a pandas df
+
+        Raises:
+            Exception: handles instances without date and returns
+            information as a json.
+        """
+        
+        if len(input_date) < 11:
+            input_date = input_date + " 00:00"
+        sql_data = DegreeDataTable.query.filter_by(datetime = input_date).count()
+        
+        if sql_data > 0:
+            pass
+        else:
+            raise Exception(f"Date {input_date} not in database")
+        
     def get(self):
         
 
@@ -48,41 +82,26 @@ class DDCorelationAnalysis(Resource):
         self.period = ast.literal_eval(self.period)
         
         # Validation of the input size
-        print(len(self.period))
-        print(len(self.consumption))
         if len(self.consumption) != len(self.period):
             return {"error": "Input parameters have different sizes"}, 400
-        
-        
+              
         
         df = pd.DataFrame.from_dict({"consumption" : self.consumption,
                                      "period" : self.period})
         
         # Validation of input dates
-        for period in df["period"]:
-            
-            if self._date_validator(period) is None:
-                return {"error" : "Date requested is out of range of DB"}, 400
-            
-        start_day = min(df["period"])
-        end_day = max(df["period"])
         
-        df["period"] = pd.to_datetime(df["period"])
-        
-        
-        df = df.set_index("period")
-        
-                                     
-      
-        COLUMNS_DATA = ['datetime',
-                        'CDD_10_5',
-                        'CDD_15_5',
-                        'CDD_18_5',
-                        'HDD_10_5',
-                        'HDD_15_5',
-                        'HDD_18_5']
-        
+        try:
+            for period in df["period"]:
+                self._date_validator(period)
+        except Exception as e:
+            msg = str(e)
+            return {"error" : msg}, 400
+                                                
         try: 
+            
+            start_day = min(df["period"])
+            end_day = max(df["period"])
 
             sql_query = f"SELECT datetime, CDD_10_5, CDD_15_5, CDD_18_5,"\
                         f"HDD_10_5, HDD_15_5, HDD_18_5 " \
@@ -93,32 +112,40 @@ class DDCorelationAnalysis(Resource):
             db_data = db.session.execute(sql_query).fetchall()
             
             dd_data = pd.DataFrame(db_data,
-                                   columns = COLUMNS_DATA)
+                                   columns = self.COLUMNS_DATA)
         except:
             return {"error" : "Data retrival failed"}, 400
+        
+        # Start of the dataframe operations
+        try:
+            df["period"] = pd.to_datetime(df["period"])
+            df = df.set_index("period")
             
-               
-        dd_data["datetime"] = pd.to_datetime(dd_data["datetime"])
-        
-        dd_data = dd_data.set_index("datetime")
-        
-        daily_dd_data = dd_data.resample("D").sum()
-        
-        final_df = df.join(daily_dd_data, how="right")
-        print(final_df)
-        
-        final_df["consumption"] = pd.to_numeric(final_df["consumption"])
-        
-        max_correlation = 0
-        max_dd = ""
-        for dd in final_df.columns[1:]:
-            x = final_df["consumption"].corr(final_df[dd], method="pearson")
-            r_sq = x**2
+            dd_data["datetime"] = pd.to_datetime(dd_data["datetime"])
             
-            if r_sq > max_correlation:
-                max_correlation = r_sq
-                max_dd = dd
+            dd_data = dd_data.set_index("datetime")
+            
+            daily_dd_data = dd_data.resample("D").sum()
+            
+            final_df = df.join(daily_dd_data, how="right")
+            
+            final_df["consumption"] = pd.to_numeric(final_df["consumption"])
+        except:
+            return {"error": "Error while processing data"}
+        
+        try:
+            max_correlation = 0
+            max_dd = ""
+            for dd in final_df.columns[1:]:
+                x = final_df["consumption"].corr(final_df[dd], method="pearson")
+                r_sq = x**2
+                
+                if r_sq > max_correlation:
+                    max_correlation = r_sq
+                    max_dd = dd
+        except:
+            return {"error" : "Error in the calculation of the " 
+                    "coefficient of determination"}
                 
         print(max_correlation, max_dd)
-        # Create a R2 calculation and generate and return the best degree data with the R2
-        return {"ok" : "Well this went well"}
+        return {"best_correlation" : {max_dd : max_correlation}}
